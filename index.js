5fa193263521a0a6716192621198eadfee3ce7d4
hiba <hibzrashed@gmail.com>
2022-05-19 16:24:07 -0500

    commit tree 36e47cbbba30bfb5537db2766bc55200005b12db

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Code your solution in this file!
+ let companyName = "Scuber";
+ //const companyName = "Scuber";
+ console.log(companyName);
+
+ let mostProfitableNeighborhood = "Chelsea";
+
+ let companyCeo = "Susan Smith";
